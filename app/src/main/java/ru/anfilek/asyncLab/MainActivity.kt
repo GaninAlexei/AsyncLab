bf43d6f50e36c252219a58a6244510fbd9e57419
WIN-R7LD53SNA8F\User <ganons@yandex.ru>
2021-01-20 22:13:11 +0300

    game created

--- app/src/main/java/ru/anfilek/asyncLab/MainActivity.kt
+++ app/src/main/java/ru/anfilek/asyncLab/MainActivity.kt
@@ -17,10 +17,9 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnStart.setOnClickListener { startHandlerThread() }
         binding.btnStop.setOnClickListener { stopHandlerThread() }
-        binding.btnAsync.setOnClickListener { startAsync() }
+        binding.btnAsync.setOnClickListener { startGame() }
         binding.btnFreeze.setOnClickListener { freeze() }
 
-        testSharedResources()
     }
 
     private fun startHandlerThread() {
@@ -30,46 +29,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun stopHandlerThread() {
-        // optional
+        if(!handlerThread?.isInterrupted!!){
+            handlerThread?.interrupt()
+        }
     }
 
-    private fun startAsync() {
-        MyAsyncTask().execute()
+    private fun startGame() {
+        testSharedResources()
     }
 
     private fun freeze() {
         Thread.sleep(9000)
     }
 
-    // DO NOT DO THIS! NEVER!
-    class MyAsyncTask : AsyncTask<String, Int, String>() {
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-        }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-//            tv.text = result
-        }
-
-        override fun onProgressUpdate(vararg values: Int?) {
-            super.onProgressUpdate(*values)
-        }
-
-        override fun onCancelled(result: String?) {
-            super.onCancelled(result)
-        }
-
-        override fun onCancelled() {
-            super.onCancelled()
-        }
-
-        override fun doInBackground(vararg params: String?): String {
-            Thread.sleep(3000)
-            return "That's all"
-        }
-    }
 }
 
 
--- app/src/main/java/ru/anfilek/asyncLab/Threads.kt
+++ app/src/main/java/ru/anfilek/asyncLab/Threads.kt
@@ -1,26 +1,61 @@
 package ru.anfilek.asyncLab
 
 import android.util.Log
+import java.lang.Thread.sleep
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
 
 fun testSharedResources() {
-//    val i : AtomicInteger = AtomicInteger(0)
-    var i = 0
+    val i: AtomicInteger = AtomicInteger(0)
 
-    val thread1 = thread {
-        for (k in 0..1_000) {
-            i++
-            Log.d("TAG", "thread1: $k")
+    var thread2 = Thread()
+    var thread1 = Thread()
+
+    thread1 = thread {
+        while (i.toInt() <= 100) {
+
+            i.getAndAdd((1..5).random())
+
+            if (i.toInt() >= 100 && thread2.isAlive) {
+                Log.d("TAG", "First thread win")
+                thread2.interrupt()
+                break
+            } else {
+                Log.d("TAG", "First thread $i")
+            }
+            try {
+                sleep(100)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
+
     }
 
-    thread {
-        for (k in 0..1_000) {
-            i++
-            Log.d("TAG", "thread2: $k")
+    thread2 = thread {
+        while (i.toInt() <= 100) {
+
+            i.getAndAdd((1..5).random())
+
+            if (i.toInt() >= 100 && thread1.isAlive) {
+                Log.d("TAG", "Second thread win")
+                thread1.interrupt()
+                break
+            } else {
+                Log.d("TAG", "Second thread $i")
+            }
+
+
+            try {
+                sleep(100)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
         }
-    }.join()
+    }
+    thread2.join()
     thread1.join()
 
-    Log.d("TAG", "thread result: $i")
 }
+
